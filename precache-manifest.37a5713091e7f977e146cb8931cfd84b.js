2cd5fa96fc9ec90bf54f0596037f4ea1b6e263f3
Pavan G R <pavangr1234@gmail.com>
2023-07-03 16:39:57 +0530

    Updates

---
+++ precache-manifest.37a5713091e7f977e146cb8931cfd84b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb000ae80d36178df7ce",
+    "url": "/EcommerceWebsite/static/js/main.eb000ae8.chunk.js"
+  },
+  {
+    "revision": "5ec523101a6dd1931633",
+    "url": "/EcommerceWebsite/static/js/1.5ec52310.chunk.js"
+  },
+  {
+    "revision": "abeadb2a5bb6aef8a981",
+    "url": "/EcommerceWebsite/static/js/runtime~main.abeadb2a.js"
+  },
+  {
+    "revision": "deb62dae48145921eae54c3b4cbd703b",
+    "url": "/EcommerceWebsite/index.html"
+  }
+];
